18c07cb7d5ef6dfbddef1f83519866e55827e14f
3000Studios <mr.jwswain@gmail.com>
2025-11-26 19:20:09 -0800

    Clean Tailwind + Fix VSCode header spam

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,3 @@
-// Copyright (c) 2025 NAME.
-// All rights reserved.
-// Unauthorized copying, modification, distribution, or use of this is prohibited without express written permission.
-
-
- Unauthorized copying, modification, distribution, or use of this is prohibited without express written permission.
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
