7cd3934ca92aa826423ea68d3b42000adee29fe5
3000Studios <mr.jwswain@gmail.com>
2026-01-08 20:15:42 -0800

    auto: 2026-01-08 20:15:41

--- middleware.ts
+++ middleware.ts
@@ -4,22 +4,20 @@ import type { NextRequest } from 'next/server';
 export function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
 
-  if (!path.startsWith('/admin')) {
-    return NextResponse.next();
-  }
+  if (path.startsWith('/admin') || path.startsWith('/vip')) {
+    if (path === '/admin/login') {
+      return NextResponse.next();
+    }
 
-  if (path === '/admin/login') {
-    return NextResponse.next();
-  }
-
-  const admin = req.cookies.get('admin');
-  if (!admin) {
-    return NextResponse.redirect(new URL('/login', req.url));
+    const admin = req.cookies.get('admin');
+    if (!admin) {
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/admin/:path*'],
+  matcher: ['/admin/:path*', '/vip/:path*'],
 };
