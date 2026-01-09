9d40882db2dd59834a3e23d06d4d9df0b86ed6ff
3000Studios <mr.jwswain@gmail.com>
2026-01-08 19:41:58 -0800

    auto: 2026-01-08 19:41:58

--- app/ui/Nav.tsx
+++ app/ui/Nav.tsx
@@ -15,7 +15,7 @@ const PUBLIC_NAV_ITEMS = [
   { label: 'Store', href: '/store' },
   { label: 'Apps', href: '/apps' },
   { label: 'Info', href: '/info' },
-  { label: 'Admin', href: '/admin' },
+  { label: 'Admin', href: '/admin/login' },
 ];
 
 export default function Nav() {
