f66e8aece8a503d078bad763a3a382ad8ddf439e
3000Studios <mr.jwswain@gmail.com>
2026-01-10 04:02:55 -0800

    auto: 2026-01-10 04:02:54

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
+import HomePage from './(public)/home/page';
+
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center flex-col">
-      <h1 className="text-4xl font-bold text-red-500">Updated by VIP voice: PIPELINE CONFIRMED</h1>
-      <p className="mt-4 text-gray-400">Time: 2026-01-08T15:49:33.896Z</p>
-    </div>
-  );
+  return <HomePage />;
 }
 
